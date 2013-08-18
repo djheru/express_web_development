bc4158d6e42bfd98a728024ec93bfa18cb019fc1
djheru <djheru@gmail.com>
2013-08-18 15:08:45 -0400

    ch01 making modules

---
+++ ch01/modules_assign_function.js
@@ -0,0 +1,14 @@
+//modules styles
+
+//assign function to exports
+module.exports = function(word){
+	var reversed = '';
+	
+	var i = word.length - 1;
+	while(i > -1){
+		var letter = word[i];
+		reversed += letter;
+		i--;
+	}
+	return reversed;
+};
