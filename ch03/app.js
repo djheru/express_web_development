503bb53921af056da3f4695925109fc7209b7f1d
Philip Damra <pdamra@box.(none)>
2013-08-19 16:46:18 -0400

    routes patterns and sequential ordering

--- ch03/app.js
+++ ch03/app.js
@@ -83,6 +83,87 @@ app.unsubscribe('/', function(req,res){
 });
 
 
+//PATTERN MATCHING IN ROUTES
+
+//string based routes
+
+//will match /abcd
+app.get('/abcd', function(req,res){
+	res.send('abcd');
+});
+
+//will match /bcde, /bde
+app.get('/bc?de', function(req,res){
+	res.send('/bc?de');
+});
+
+//will match /cdef, /cddddddef
+app.get('/cd+ef', function(req,res){
+	res.send('/cd+ef');
+});
+
+//will match /efxyzgh
+app.get('/ef*gh', function(req,res){
+	res.send('/bc?de');
+});
+
+//will match /fgj and /fghij
+app.get('/fg(hi)?j', function(req,res){
+	res.send('/fg(hi)?j');
+});
+
+//Named Placeholders
+app.get('/user/:id', function(req, res){
+	res.send('User ID: ' + req.params.id);
+});
+
+app.get('/country/:country/state/:state',function(req,res){
+	res.send(req.params.country + ' - ' + req.params.state);
+});
+
+//named placeholders with special characters
+app.get('/route/:from-:to', function(req,res){
+	res.send("From: " + req.params.from + " - TO: " + req.param.to);
+});
+
+app.get('/file/:name.:ext', function(req,res){
+	res.send("File: " + req.params.name + '.' + req.params.ext.toLowerCase());
+});
+
+//Pattern matching with named placeholders
+
+//optional named parameters
+app.get('/feed/:format?', function(req,res){
+	if(req.params.format){
+		res.send('Format: ' + req.params.format);
+	}else{
+		res.send('default format');
+	}
+});
+
+//Route order of precedence
+
+app.get('/ab*', function(req,res){
+	res.send('ab*');
+});
+app.get('/abc', function(req,res){//only this gets executed
+	res.send('abc');
+});
+
+//Use next() to get more matching 
+app.get('/xy*', function(req,res, next){
+	//if the path is /xyz, dont handle it, pass it along
+	if(req.path == '/xyz'){
+		next();
+	}else{
+		res.send('/xy*');
+	}
+});
+app.get('/xyz', function(req,res){//only this gets executed
+	res.send('xyz');
+});
+
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
