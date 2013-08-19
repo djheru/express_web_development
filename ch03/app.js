77a370ee9410db673776839cd4e982e9119bdb8f
Philip Damra <pdamra@box.(none)>
2013-08-19 14:52:57 -0400

    Test ALL the verbs

--- ch03/app.js
+++ ch03/app.js
@@ -28,9 +28,61 @@ app.use(express.static(path.join(__dirname, 'public')));
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
-
+/* Bootstrap code
 app.get('/', routes.index);
 app.get('/users', user.list);
+*/
+
+/*
+//Catch all
+app.all('/', function(req,res,next){
+	res.set('X-Catch-All', 'true');
+	next();
+});
+*/
+
+//GET
+app.get('/', function(req,res){
+	res.send('/ GET OK');
+});
+//POST
+app.post('/', function(req,res){
+	res.send('/ POST OK');
+});
+//PUT
+app.put('/', function(req,res){
+	res.send('/ PUT OK');
+});
+//PATCH
+app.patch('/', function(req,res){
+	res.send('/ PATCH OK');
+});
+//DELETE
+app.delete('/', function(req,res){
+	res.send('/ DELETE OK');
+});
+//OPTIONS
+app.options('/', function(req,res){
+	res.send('/ OPTIONS OK');
+});
+//M-SEARCH
+app['m-search']('/', function(req,res){
+	res.send('/ M-SEARCH OK');
+});
+//NOTIFY
+app.notify('/', function(req,res){
+	res.send('/ NOTIFY OK');
+});
+//SUBSCRIBE
+app.subscribe('/', function(req,res){
+	res.send('/ SUBSCRIBE OK');
+});
+//UNSUBSCRIBE
+app.unsubscribe('/', function(req,res){
+	res.send('/ UNSUBSCRIBE OK');
+});
+
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
