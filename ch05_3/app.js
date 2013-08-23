47a7aec5e028c9b1de01460c8aaded7933aa48ff
Philip Damra <pdamra@box.(none)>
2013-08-23 15:10:28 -0400

    jade include and extend

--- ch05_3/app.js
+++ ch05_3/app.js
@@ -28,6 +28,8 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
+app.get('/includes', routes.include);
+app.get('/inherits', routes.inherit);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
--- ch05_3/routes/index.js
+++ ch05_3/routes/index.js
@@ -6,3 +6,9 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+exports.include = function(req, res){
+  res.render('include');
+};
+exports.inherit = function(req, res){
+  res.render('inherit');
+};
